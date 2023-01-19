363de15cda4ff18cba31bb4089e1caae0319f9ab
unknown <mailson.ribeiro@ufrn.br>
2023-01-19 13:46:08 -0300

    Atualização Method Reference

--- src/lambadas/Foreach.java
+++ src/lambadas/Foreach.java
@@ -12,8 +12,16 @@ public class Foreach {
 		}
 		System.out.println("\nLambda #01");
 		aprovados.forEach(nome -> System.out.println(nome + "!!!"));
-		System.out.println("\nMethod reference...");
+		System.out.println("\nMethod reference #01");
 		aprovados.forEach(System.out::println);
+		
+		System.out.println("\nLambda #02");
+		aprovados.forEach(nome -> meuImprimir(nome));
+		System.out.println("\nMethod reference #02");
+		aprovados.forEach(Foreach::meuImprimir);
+	}
+	static void meuImprimir(String nome) {
+		System.out.println("Meu nome é "+ nome);
 	}
 
 }
